1752de7452cc0dd7d0a4572ab657880776d01d9e
Andrea Froncillo <andreafroncillo0@gmail.com>
2025-02-20 19:05:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-let drink = prompt(`Cosa vuoi da bere? 1. Acqua; 2. Coca-Cola; 3. Birra`);
 
-
-
-if(drink == 1){
-    console.log(`E’ stata selezionata l’acqua`);
-} else if(drink == 2){
-    console.log(`E’ stata selezionata coca cola`);
-} else if(drink == 3){
-    console.log(`E’ stata selezionata birra`);
-} else{
-    while(drink != 1, 2, 3){
-        prompt(`Cosa vuoi da bere? 1. Acqua; 2. Coca-Cola; 3. Birra`)
-    }
-};
 
 
 
